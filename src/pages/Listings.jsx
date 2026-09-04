18aa11ce0063076305e03dfe43a33368dc00627b
dale <gagikapre09@gmail.com>
2026-09-04 19:29:15 +0800

    matching of listing and listing images to match the info and the image output

--- src/pages/Listings.jsx
+++ src/pages/Listings.jsx
@@ -3,6 +3,7 @@ import {supabase} from "../supabaseClient"
 
 function Listings(){
     const[listings, setListings] = useState([])
+    const[images, setImages]= useState([])
     const [loading, setLoading] = useState(true)
 
 // gets the stored data from listing in supabase
@@ -31,15 +32,58 @@ function Listings(){
             }
             
         checkListings()
+
+
+
+        // gets the stored images from listings_images in supabase
+      async function getListingImages() {
+        try{
+        const{data,error} = await supabase
+        .from('listings_images')
+        .select('*')
+
+
+        if(error){
+            console.error("failed getting the images", error.message);
+            setLoading(false)
+            return
+        }
+
+        setImages(data)
+        setLoading(false)
+    } catch (err){
+        console.error("Unexpected error:", err.message)
+        setLoading(false)
+    }
         
+      }
+getListingImages()
+
+
+
+
 
 
     }, [])
+    
 
 
     return(
         <div>
             <h1>Listings</h1>
+
+
+                {listings.map((listing) => {
+                    const image = images.find((img) => img.listing_id === listing.id)
+                    return(
+                        <div key={listing.id}>
+                        <p className="listing-title">{listing.title}</p>
+                        <img src={image.images_url} alt={listing.title}/>
+                        <p className="price"> {listing.price}</p>
+                        <p className="item-description">{listing.description}</p>
+                        </div>
+                    )
+                })}
         </div>
     )
 }
